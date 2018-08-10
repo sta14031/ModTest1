51eedfcb11587e11e484ac1988a4c0c1c7f75936
Tyler Starr <sta14031@byui.edu>
2018-08-10 12:24:17 -0700

    Added mod class

---
+++ src/main/java/com/lkrsfn/test1/ModTest1.java
@@ -0,0 +1,28 @@
+package com.lkrsfn.test1;
+
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+
+@Mod(modid = "modtest1", name = "Test 1", version = "1.12.2-1.0")
+public class ModTest1 {
+
+    @Mod.Instance("modtest1")
+    public static ModTest1 instance;
+
+    @Mod.EventHandler
+    public void preInit(FMLPreInitializationEvent event) {
+
+    }
+
+    @Mod.EventHandler
+    public void init(FMLInitializationEvent event) {
+
+    }
+
+    @Mod.EventHandler
+    public void postInit(FMLPostInitializationEvent event) {
+
+    }
+}
